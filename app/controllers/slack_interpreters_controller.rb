7ddaa65155e1841a25559bf401d00c89a3f90b51
Hunter Fortuin <hunter@flitti.com>
2015-05-30 23:18:27 -0400

    remove redirects

--- app/controllers/slack_interpreters_controller.rb
+++ app/controllers/slack_interpreters_controller.rb
@@ -2,36 +2,54 @@ class SlackInterpretersController < ApplicationController
     skip_before_action :authenticate_user!
 
     def tasks
-        if params[:text].include?("showalltasks")
-            redirect_to all_tasks_slack_interpreters_url(params)
-        else
-            new_task
+        if params[:text].include?("showalltasks") # show all tasks
+            team = Team.find_by_slack_token(params[:token])
+            user = team.present? ? team.users.find_by_slack_username(params[:user_name]) : nil
+
+            if user.present?
+                render status: 200, json: "#{user.tasks.where(complete: false).map { |t| "* #{t.description}" }}"
+            else
+                render status: 200, json: "Your configuration is messed up. Make sure your token and slack username are correct."
+            end
+        else # new task
+            team = Team.find_by_slack_token(params[:token])
+            user = team.present? ? team.users.find_by_slack_username(params[:user_name]) : nil
+            user_id = user.present? ? user.id : nil
+            description = params[:text]
+
+            task = Task.new(description: description, user_id: user_id)
+
+            if task.save
+                render status: 200, json: "#{task.description} has been added to your to do list."
+            else
+                render status: :unprocessable_entity, json: "#{task.errors.full_messages.to_sentence} - Make sure everything is configured correctly."
+            end
         end
     end
 
-    def new_task
-        team = Team.find_by_slack_token(params[:token])
-        user = team.present? ? team.users.find_by_slack_username(params[:user_name]) : nil
-        user_id = user.present? ? user.id : nil
-        description = params[:text]
+    # def new_task
+    #     team = Team.find_by_slack_token(params[:token])
+    #     user = team.present? ? team.users.find_by_slack_username(params[:user_name]) : nil
+    #     user_id = user.present? ? user.id : nil
+    #     description = params[:text]
 
-        task = Task.new(description: description, user_id: user_id)
+    #     task = Task.new(description: description, user_id: user_id)
 
-        if task.save
-            render status: 200, json: "#{task.description} has been added to your to do list."
-        else
-            render status: :unprocessable_entity, json: "#{task.errors.full_messages.to_sentence} - Make sure everything is configured correctly."
-        end
-    end
+    #     if task.save
+    #         render status: 200, json: "#{task.description} has been added to your to do list."
+    #     else
+    #         render status: :unprocessable_entity, json: "#{task.errors.full_messages.to_sentence} - Make sure everything is configured correctly."
+    #     end
+    # end
 
-    def all_tasks
-        team = Team.find_by_slack_token(params[:token])
-        user = team.present? ? team.users.find_by_slack_username(params[:user_name]) : nil
+    # def all_tasks
+    #     team = Team.find_by_slack_token(params[:token])
+    #     user = team.present? ? team.users.find_by_slack_username(params[:user_name]) : nil
 
-        if user.present?
-            render status: 200, json: "#{user.tasks.where(complete: false).map { |t| "* #{t.description}" }}"
-        else
-            render status: 200, json: "Your configuration is messed up. Make sure your token and slack username are correct."
-        end
-    end
+    #     if user.present?
+    #         render status: 200, json: "#{user.tasks.where(complete: false).map { |t| "* #{t.description}" }}"
+    #     else
+    #         render status: 200, json: "Your configuration is messed up. Make sure your token and slack username are correct."
+    #     end
+    # end
 end
